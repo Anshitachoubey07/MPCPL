07e79aa99c29593196f289cac103e258919fb6c7
Anshita Choubey <92245615+Anshitachoubey07@users.noreply.github.com>
2025-02-09 18:28:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,32 @@
-// Placeholder for future JavaScript functionality
-// Example: Scroll to sections on menu click
+const dropdown = document.querySelector('.dropdown');
+const navList = document.querySelector('nav ul');
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const links = document.querySelectorAll('nav ul li a');
-  
-//     links.forEach(link => {
-//       link.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         const targetId = link.getAttribute('href').replace('.html', '').replace('#', '');
-//         const targetElement = document.getElementById(targetId);
-//         if (targetElement) {
-//           window.scrollTo({
-//             top: targetElement.offsetTop - 50, // Adjust for header height
-//             behavior: 'smooth'
-//           });
-//         }
-//       });
-//     });
-//   });
-  
+dropdown.addEventListener('click', () => {
+    navList.classList.toggle('show');
+});
+
+
 function sendMail() {
-  var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    phone: document.getElementById("phone").value,
-    message: document.getElementById("message").value,
-  };
-
-  const serviceID = "service_d0r6hmq";
-  const templateID = "template_w6e46af";
-
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("phone").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
-    })
-    .catch(err=>console.log(err));
-
-}
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_d0r6hmq";
+    const templateID = "template_w6e46af";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("phone").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
